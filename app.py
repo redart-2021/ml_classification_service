efca35f7c47517c84cd29d5bcf0edb6cb5bec536
Alexandr.S <sosnin_as@ekb.sima-land.ru>
2021-09-04 14:18:28 +0500

    init

---
+++ app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from flask import Flask
+from core.active_classificator import ActiveClassificator
+from datetime import date
+from flask import request
+import json
+
+app = Flask(__name__)
+act_class = ActiveClassificator(file_name="core/models/knn_model.model")
+
+@app.route('/')
+def servise_status():
+    return f'Service status: OK! used model: {act_class.model_name}'
+
+@app.route('/predict', methods=['POST'])
+def get_predict():
+    req = dict(request.json)
+    return str(act_class.predict([[req["right_fields"], req["wrong_fields"], req["user_stats"]]])[0])
+
+@app.route('/fit', methods=['POST'])
+def fit(data):
+    data = pd.json_normalize(json.loads(data))
+    return act_class.fit(data[["lead_time", "stats"]].to_numpy())
+
+if __name__ == '__main__':
+    app.run()
---
+++ core/active_classificator.py
@@ -0,0 +1,20 @@
+from sklearn.cluster import KMeans
+import pickle
+from datetime import date
+
+
+class ActiveClassificator:
+    def __init__(self, file_name):
+        self.model = pickle.load(open(file_name, 'rb'))
+        self.model_name = file_name
+
+    def get_model_name(self):
+        return self.model_name
+
+    def predict(self, data):
+        return self.model.predict(data)
+
+    def fit(self, data, n_clusters=6, random_state=0):
+        self.model = KMeans(n_clusters=n_clusters, random_state=random_state).fit(data)
+        pickle.dump(self.model, open(f"{date.today()}_classificator.model", 'wb'))
+
